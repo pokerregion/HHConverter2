3fc0388754c563281219a87278970fc30d6499e5
bclayman <bclayman@gmail.com>
2015-09-19 21:46:24 -0400

    basic improvements to code quality, commenting

--- src/HandHistoryConverter.java
+++ src/HandHistoryConverter.java
@@ -49,56 +49,50 @@ public class HandHistoryConverter {
 			BufferedWriter writer, Player player1Seat, Player player2Seat,
 			int buttonSeat) throws IOException {
 
+		//initialize player and dealer objects
 		Player player1 = handHistory.getPlayer1();
 		Player player2 = handHistory.getPlayer2();
 		Dealer dealer = handHistory.getDealer();
 
+		//write header line for hand
 		writer.write(String.format(HEADER, buttonSeat, player1Seat.getName(),
 				player2Seat.getName()));
 
-		// if (player1.getIsDealer()) {
-		// writer.write(String.format(HEADER, player2.getName(),
-		// player1.getName()));
-		// } else {
-		// writer.write(String.format(HEADER, player1.getName(),
-		// player2.getName()));
-		// }
-		// print starting information (table specific)
-		// print seats and chip stacks
-		// print blind postings
-		// print *** HOLE CARDS ***
+		//set up blinds
 		writeBlind(player2, writer);
 		writeBlind(player1, writer);
 
+		//set up hole cards
 		writeHoleCards(player1, writer);
 		writeHoleCards(player2, writer);
 
 		// PREFLOP ACTION
 		writeActions(player1, player1.getPreflopActions(), player2,
 				player2.getPreflopActions(), true, writer);
-		// writePreflopAction(player1, player2, writer);
+
 		// print flop
 		if (dealer != null) {
+			//print flop cards
 			writeFlop(dealer, writer);
-			// FLOP ACTION
+			// flop actions
 			writeActions(player1, player1.getFlopActions(), player2,
 					player2.getFlopActions(), false, writer);
-			// writeFlopAction(player1, player2, writer);
+
 			// print turn
 			if (dealer.hasTurn()) {
+				//print turn card
 				writeTurn(dealer, writer);
-				// TURN ACTION
+				// turn actions
 				writeActions(player1, player1.getTurnActions(), player2,
 						player2.getTurnActions(), false, writer);
-				// writeTurnAction(player1, player2, writer);
+
 				// print river
 				if (dealer.hasRiver()) {
+					//print river card
 					writeRiver(dealer, writer);
-					// RIVER ACTION
+					// print river actions
 					writeActions(player1, player1.getRiverActions(), player2,
 							player2.getRiverActions(), false, writer);
-					// writeRiverAction(player1, player2, writer);
-					
 				}
 			}
 		}
@@ -106,6 +100,7 @@ public class HandHistoryConverter {
 		writeChangeInStackSummary(player1, player2, writer);
 	}
 
+	//summary line about collecting x from pot (mostly for Holdem Manager)
 	public static void writeChangeInStackSummary(Player player1, Player player2, BufferedWriter writer) throws IOException {
 		writer.write("*** SHOW DOWN ***\n");
 		if (player1.getChangeInStack() > 0) {
@@ -115,6 +110,7 @@ public class HandHistoryConverter {
 		}
 	}
 	
+	//posting blinds
 	public static void writeBlind(Player player, BufferedWriter writer)
 			throws IOException {
 		int bet = player.getCurrentBet();
@@ -123,6 +119,7 @@ public class HandHistoryConverter {
 		writer.newLine();
 	}
 
+	//determine correct arguments for second writeActions function (below)
 	public static void writeActions(Player player1,
 			List<Action> player1Actions, Player player2,
 			List<Action> player2Actions, boolean dealerFirst,
@@ -146,13 +143,16 @@ public class HandHistoryConverter {
 		}
 	}
 
+	//function to go back and forth between players, writing their actions
 	private static void writeActions(Player player1,
 			List<Action> player1Actions, Player player2,
 			List<Action> player2Actions, BufferedWriter writer)
 			throws IOException {
+		//as long as player 1 has another action
 		while (!player1Actions.isEmpty()) {
 			Action next1 = player1Actions.remove(0);
 			writeNextAction(player1, next1, writer);
+			//if player 2 has another action
 			if (!player2Actions.isEmpty()) {
 				Action next2 = player2Actions.remove(0);
 				writeNextAction(player2, next2, writer);
@@ -160,35 +160,35 @@ public class HandHistoryConverter {
 		}
 	}
 
-	// preflop action (dealer first)
+	//preflop action (dealer first)
 	public static void writePreflopAction(Player player1, Player player2,
 			BufferedWriter writer) throws IOException {
-		// if player 1 is dealer, he acts first
+		//if player 1 is dealer, he acts first
 		if (player1.getIsDealer()) {
 			while (!player1.getPreflopActions().isEmpty()
 					|| !player2.getPreflopActions().isEmpty()) {
 				if (!player1.getPreflopActions().isEmpty()) {
-					// get next player 1 preflop action
+					//get next player 1 preflop action
 					Action next = player1.getNextPreflopAction();
 					writeNextAction(player1, next, writer);
 				}
 				if (!player2.getPreflopActions().isEmpty()) {
-					// get next player 2 preflop action
+					//get next player 2 preflop action
 					Action next = player2.getNextPreflopAction();
 					writeNextAction(player2, next, writer);
 				}
 			}
-			// player 2 is dealer
+			//player 2 is dealer
 		} else {
 			while (!player1.getPreflopActions().isEmpty()
 					|| !player2.getPreflopActions().isEmpty()) {
 				if (!player2.getPreflopActions().isEmpty()) {
-					// get next player 2 preflop action
+					//get next player 2 preflop action
 					Action next = player2.getNextPreflopAction();
 					writeNextAction(player2, next, writer);
 				}
 				if (!player1.getPreflopActions().isEmpty()) {
-					// get next player 1 preflop action
+					//get next player 1 preflop action
 					Action next = player1.getNextPreflopAction();
 					writeNextAction(player1, next, writer);
 				}
@@ -196,35 +196,35 @@ public class HandHistoryConverter {
 		}
 	}
 
-	// flop action (dealer acts second)
+	//flop action (dealer acts second)
 	public static void writeFlopAction(Player player1, Player player2,
 			BufferedWriter writer) throws IOException {
-		// player 1 acts first
+		//player 1 acts first
 		if (player2.getIsDealer()) {
 			while (!player1.getFlopActions().isEmpty()
 					|| !player2.getFlopActions().isEmpty()) {
 				if (!player1.getFlopActions().isEmpty()) {
-					// get next player 1 preflop action
+					//get next player 1 preflop action
 					Action next = player1.getNextFlopAction();
 					writeNextAction(player1, next, writer);
 				}
 				if (!player2.getFlopActions().isEmpty()) {
-					// get next player 2 preflop action
+					//get next player 2 preflop action
 					Action next = player2.getNextFlopAction();
 					writeNextAction(player2, next, writer);
 				}
 			}
-			// player 2 acts first
+			//player 2 acts first
 		} else {
 			while (!player1.getFlopActions().isEmpty()
 					|| !player2.getFlopActions().isEmpty()) {
 				if (!player2.getFlopActions().isEmpty()) {
-					// get next player 2 preflop action
+					//get next player 2 preflop action
 					Action next = player2.getNextFlopAction();
 					writeNextAction(player2, next, writer);
 				}
 				if (!player1.getFlopActions().isEmpty()) {
-					// get next player 1 preflop action
+					//get next player 1 preflop action
 					Action next = player1.getNextFlopAction();
 					writeNextAction(player1, next, writer);
 				}
@@ -232,35 +232,35 @@ public class HandHistoryConverter {
 		}
 	}
 
-	// turn action (dealer acts second)
+	//turn action (dealer acts second)
 	public static void writeTurnAction(Player player1, Player player2,
 			BufferedWriter writer) throws IOException {
-		// player 1 acts first
+		//player 1 acts first
 		if (player2.getIsDealer()) {
 			while (!player1.getTurnActions().isEmpty()
 					|| !player2.getTurnActions().isEmpty()) {
 				if (!player1.getTurnActions().isEmpty()) {
-					// get next player 1 preflop action
+					//get next player 1 preflop action
 					Action next = player1.getNextTurnAction();
 					writeNextAction(player1, next, writer);
 				}
 				if (!player2.getTurnActions().isEmpty()) {
-					// get next player 2 preflop action
+					//get next player 2 preflop action
 					Action next = player2.getNextTurnAction();
 					writeNextAction(player2, next, writer);
 				}
 			}
-			// player 2 acts first
+			//player 2 acts first
 		} else {
 			while (!player1.getTurnActions().isEmpty()
 					|| !player2.getTurnActions().isEmpty()) {
 				if (!player2.getTurnActions().isEmpty()) {
-					// get next player 2 preflop action
+					//get next player 2 preflop action
 					Action next = player2.getNextTurnAction();
 					writeNextAction(player2, next, writer);
 				}
 				if (!player1.getTurnActions().isEmpty()) {
-					// get next player 1 preflop action
+					//get next player 1 preflop action
 					Action next = player1.getNextTurnAction();
 					writeNextAction(player1, next, writer);
 				}
@@ -268,35 +268,35 @@ public class HandHistoryConverter {
 		}
 	}
 
-	// river action (dealer acts second)
+	//river action (dealer acts second)
 	public static void writeRiverAction(Player player1, Player player2,
 			BufferedWriter writer) throws IOException {
-		// player 1 acts first
+		//player 1 acts first
 		if (player2.getIsDealer()) {
 			while (!player1.getRiverActions().isEmpty()
 					|| !player2.getRiverActions().isEmpty()) {
 				if (!player1.getRiverActions().isEmpty()) {
-					// get next player 1 preflop action
+					//get next player 1 preflop action
 					Action next = player1.getNextRiverAction();
 					writeNextAction(player1, next, writer);
 				}
 				if (!player2.getRiverActions().isEmpty()) {
-					// get next player 2 preflop action
+					//get next player 2 preflop action
 					Action next = player2.getNextRiverAction();
 					writeNextAction(player2, next, writer);
 				}
 			}
-			// player 2 acts first
+			//player 2 acts first
 		} else {
 			while (!player1.getRiverActions().isEmpty()
 					|| !player2.getRiverActions().isEmpty()) {
 				if (!player2.getRiverActions().isEmpty()) {
-					// get next player 2 preflop action
+					//get next player 2 preflop action
 					Action next = player2.getNextRiverAction();
 					writeNextAction(player2, next, writer);
 				}
 				if (!player1.getRiverActions().isEmpty()) {
-					// get next player 1 preflop action
+					//get next player 1 preflop action
 					Action next = player1.getNextRiverAction();
 					writeNextAction(player1, next, writer);
 				}
@@ -304,7 +304,7 @@ public class HandHistoryConverter {
 		}
 	}
 
-	// write next action
+	//write next action
 	public static void writeNextAction(Player player, Action actionObj,
 			BufferedWriter writer) throws IOException {
 		String action = actionObj.getAction();
@@ -322,29 +322,29 @@ public class HandHistoryConverter {
 		}
 	}
 
-	// functions for writing a line for each of fold, check, call, all-in call,
-	// bet, all-in bet, raise, all-in raise
+	/*functions for writing a line for each of fold, check, call, all-in call,
+	bet, all-in bet, raise, all-in raise*/
 
-	// fold
+	//fold
 	public static void writeFold(Player player, BufferedWriter writer)
 			throws IOException {
 		writer.write(player.getName() + ": folds\n");
 	}
 
-	// check
+	//check
 	public static void writeCheck(Player player, BufferedWriter writer)
 			throws IOException {
 		writer.write(player.getName() + ": checks\n");
 	}
 
-	// call
+	//call
 	public static void writeCall(Player player, int amount,
 			BufferedWriter writer) throws IOException {
 		writer.write(player.getName() + ": calls " + amount + "\n");
 		player.setCurrentBet(amount);
 	}
 
-	// all-in call
+	//all-in call
 	public static void writeAllInCall(Player player, int amount,
 			BufferedWriter writer) throws IOException {
 		writer.write(player.getName() + ": calls " + amount
@@ -352,28 +352,28 @@ public class HandHistoryConverter {
 		player.setCurrentBet(amount);
 	}
 
-	// bet
+	//bet
 	public static void writeBet(Player player, int amount, BufferedWriter writer)
 			throws IOException {
 		writer.write(player.getName() + ": bets " + amount + "\n");
 		player.setCurrentBet(amount);
 	}
 
-	// all-in bet
+	//all-in bet
 	public static void writeAllInBet(Player player, int amount,
 			BufferedWriter writer) throws IOException {
 		writer.write(player.getName() + ": bets " + amount + " and is all-in\n");
 		player.setCurrentBet(amount);
 	}
 
-	// uncalled bet
+	//uncalled bet
 	public static void writeUncalledBet(Player player, int amount,
 			BufferedWriter writer) throws IOException {
 		writer.write("Uncalled bet (" + amount + ") returned to "
 				+ player.getName() + "\n");
 	}
 
-	// raise
+	//raise
 	public static void writeRaise(Player player, int raiseAmount,
 			int raiseTotal, BufferedWriter writer) throws IOException {
 		writer.write(player.getName() + ": raises " + raiseAmount + " to "
@@ -381,7 +381,7 @@ public class HandHistoryConverter {
 		player.setCurrentBet(raiseTotal);
 	}
 
-	// all-in raise
+	//all-in raise
 	public static void writeAllInRaise(Player player, int raiseAmount,
 			int raiseTotal, BufferedWriter writer) throws IOException {
 		writer.write(player.getName() + ": raises " + raiseAmount + " to "
@@ -389,7 +389,7 @@ public class HandHistoryConverter {
 		player.setCurrentBet(raiseTotal);
 	}
 
-	// each hole card string will be 4 characters
+	//each hole card string will be 4 characters
 	public static void writeHoleCards(Player player, BufferedWriter writer)
 			throws IOException {
 		String holeCards = player.getHoleCards();
@@ -399,6 +399,8 @@ public class HandHistoryConverter {
 				+ card2 + "]\n");
 	}
 
+	//basic functions for writing flop, turn, and river
+	
 	public static void writeFlop(Dealer dealer, BufferedWriter writer)
 			throws IOException {
 		String[] flop = dealer.getFlop();
